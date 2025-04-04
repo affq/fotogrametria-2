b81380fd1d302a08b51129f603a52a631d38f7d2
affq <adrianfabisiewicz@gmail.com>
2025-04-04 16:25:29 +0200

    Update outlier detection parameters and adjust point cloud processing flow

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def las_to_o3d(plik) :
     chmura_punktow.colors = o3d.utility.Vector3dVector(las_colors)
     return chmura_punktow
 
-def wyznaczanie_obserwacji_odstajacych(chmura_punktow, liczba_sasiadow = 20, std_ratio = 3.0):
+def wyznaczanie_obserwacji_odstajacych(chmura_punktow, liczba_sasiadow = 1000, std_ratio = 3.0):
     chmura_punktow_odfiltrowana, ind = chmura_punktow.remove_statistical_outlier(nb_neighbors = liczba_sasiadow, std_ratio = std_ratio)
     punkty_odstajace = chmura_punktow.select_by_index(ind, invert = True)
     punkty_odstajace.paint_uniform_color([1, 0, 0])
@@ -42,11 +42,11 @@ if __name__ == "__main__":
     # o3d.visualization.draw_geometries([chmura_punktow], window_name = "Chmura punktów")
 
     chmura_punktow_odfiltrowana, punkty_odstajace = wyznaczanie_obserwacji_odstajacych(chmura_punktow)
-    o3d.visualization.draw_geometries([chmura_punktow_odfiltrowana, punkty_odstajace], window_name = "Chmura punktów z obserwacjami odstającymi")
+    # o3d.visualization.draw_geometries([chmura_punktow_odfiltrowana, punkty_odstajace], window_name = "Chmura punktów z obserwacjami odstającymi")
 
-    chmura_punktow_woksele = regularyzacja_chmur_punktow(chmura_punktow, odleglosc_miedzy_wokselami = 0.1)
+    chmura_punktow_woksele = regularyzacja_chmur_punktow(chmura_punktow_odfiltrowana, odleglosc_miedzy_wokselami = 0.1)
     o3d.visualization.draw_geometries([chmura_punktow_woksele], window_name = "Chmura punktów woksele")
 
-    chmura_punktow_co_nty = usuwanie_co_ntego(chmura_punktow, n = 10)
-    o3d.visualization.draw_geometries([chmura_punktow_co_nty], window_name = "Chmura punktów co n-tego")
+    # chmura_punktow_co_nty = usuwanie_co_ntego(chmura_punktow, n = 10)
+    # o3d.visualization.draw_geometries([chmura_punktow_co_nty], window_name = "Chmura punktów co n-tego")
     
